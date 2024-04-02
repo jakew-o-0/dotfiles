7dbb2cc0ede0ee0818216fff2cbc406a0bc0ff6c
jakew-o-0 <jakewbailey007@gmail.com>
2024-04-02 14:38:22 +0100

    updated install scripts

---
+++ scripts/system-setup.sh
@@ -0,0 +1,43 @@
+#/bin/bash
+
+# installing/setting-up yay
+pacman -S --needed --noconfirm git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+yay -Y --noconfirm --gendb
+yay -Syu --noconfirm --devel --save
+
+
+# PKGBUILD config
+yay -S --noconfirm mold pigz pbzip2 lbzip2 plzip
+sudo mv ~/linux_configs/arch-sys-config/makepkg.conf /etc/makepkg.conf
+
+
+
+
+
+
+# install
+read -sp "Install Desktop [y/n]: " installDesktop
+read -sp "Install Workstation [y/n]: " installWorkstation
+
+
+if [[ $installDesktop -eq "y" ]]; then
+    yay -S --noconfirm hyprland hyprpaper rofi-lbonn-wayland kitty ttf-martian-mono-nerd
+fi
+
+if [[ $installWorkstation -eq "y" ]]; then
+    yay -S --noconfirm neovim tmux zsh starship fzf
+    chsh -s /usr/bin/zsh jake
+
+    # oh-my-zsh
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    rm ~/.zshrc
+
+    # tpm for tmux
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+    stow .
+fi
