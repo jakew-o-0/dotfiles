99bcfadb4ccfd338b7ef90fadd640ca9df91f105
jakew-o-0 <jakewbailey007@gmail.com>
2024-05-17 01:20:49 +0100

    updated nvim conf

--- .config/nvim/lua/plugins/colourscheme.lua
+++ .config/nvim/lua/plugins/colourscheme.lua
@@ -2,9 +2,6 @@ return {
   'rebelot/kanagawa.nvim',
   priority = 1000,
   config = function()
-    require('kanagawa').setup({
-      --transparent = true,
-    })
     vim.cmd("colorscheme kanagawa")
   end,
 }
