b0a3b278baf62cd7088f2cf5731c837544e94d30
jakew-o-0 <jakewbailey007@gmail.com>
2024-05-18 11:58:56 +0100

    updated install script

--- scripts/system-setup.sh
+++ scripts/system-setup.sh
@@ -1,43 +1,33 @@
 #/bin/bash
 
 # installing/setting-up yay
-pacman -S --needed --noconfirm git base-devel
+sudo pacman -S --needed --noconfirm git base-devel
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
-
 yay -Y --noconfirm --gendb
 yay -Syu --noconfirm --devel --save
+cd ~
 
 
 # PKGBUILD config
 yay -S --noconfirm mold pigz pbzip2 lbzip2 plzip
-sudo mv ~/linux_configs/arch-sys-config/makepkg.conf /etc/makepkg.conf
+sudo mv ~/dotfiles/arch-sys-config/makepkg.conf /etc/makepkg.conf
 
 
-
-
-
-
-# install
+# install packages and setup config files
 read -sp "Install Desktop [y/n]: " installDesktop
 read -sp "Install Workstation [y/n]: " installWorkstation
 
-
 if [[ $installDesktop -eq "y" ]]; then
-    yay -S --noconfirm hyprland hyprpaper rofi-lbonn-wayland kitty ttf-martian-mono-nerd
+    yay -S --noconfirm hyprland hyprpaper rofi-lbonn-wayland kitty ttf-jetbrains-mono-nerd
 fi
 
 if [[ $installWorkstation -eq "y" ]]; then
-    yay -S --noconfirm neovim tmux zsh starship fzf stow
+    yay -S --noconfirm neovim tmux zsh fzf ripgrep stow
     chsh -s /usr/bin/zsh jake
-
-    # oh-my-zsh
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    rm ~/.zshrc
-
     # tpm for tmux
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
+    cd ~/dotfiles
     stow .
 fi
